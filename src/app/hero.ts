5d8a15e08525422146ae2d68bfd7ca0deb8d8693
psg <skpar16@naver.com>
2019-01-12 17:15:57 +0900

    바인딩 – PROPERTY 바인딩 model -> view 로의 바인딩하는 property 바인딩을 알아보자. property 바인딩에는 attrbute 바인딩, class 바인딩, style 바인딩 크게 세가지가 있다. 각각 살펴보자.

---
+++ src/app/hero.ts
@@ -0,0 +1,3 @@
+export class Hero {
+  constructonr(id: number, name: string)
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  // 1) 리터럴 객체로 객체 생성
+  // hero: Hero = {
+  //   id:11,
+  //   name: 'winstors'
+  // }
+  hero: Hero;
 
-  constructor() { }
+  isSpecial = false;
+
+  constructor() {
+    // 2) new 키워드로 객체 생성
+    this.hero = new Hero(11, 'winstorm');
+    // id, name은 무슨 값이 할당되는가?
+    console.log('hero;' , this.hero);
+  }
 
   ngOnInit() {
   }
